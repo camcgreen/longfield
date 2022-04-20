931289212e5b2d7923b453345aeef19f01f4f8e7
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-04-20 11:18:02 +0100

    add max width to our team page

--- pages/the-team.js
+++ pages/the-team.js
@@ -14,211 +14,216 @@ const Team = () => {
       </Head>
       <Header />
       <main className={styles.team}>
-        <h1>Our team</h1>
-        <p>
-          We strive to ensure that all of our team members uphold our core
-          values, putting these at the heart of every decision they make.
-        </p>
-        <p>
-          We want everyone at Longfield to feel proud of the work they do, the
-          company they work for and the difference that they make.
-        </p>
-        <div className={styles.photos}>
-          <div className={styles.row3}>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img
-                  src='/stephen.jpg'
-                  alt='Stephen Kilburn - Chief Operating Officer'
-                />
-                <div className={styles.overlay}>
+        <div className='wrapper'>
+          <h1>Our team</h1>
+          <p>
+            We strive to ensure that all of our team members uphold our core
+            values, putting these at the heart of every decision they make.
+          </p>
+          <p>
+            We want everyone at Longfield to feel proud of the work they do, the
+            company they work for and the difference that they make.
+          </p>
+          <div className={styles.photos}>
+            <div className={styles.row3}>
+              <div className={styles.profile}>
+                <div className={styles.image}>
                   <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
+                    src='/stephen.jpg'
+                    alt='Stephen Kilburn - Chief Operating Officer'
                   />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Stephen Kilburn</h2>
+                <h2>Chief Operating Officer</h2>
               </div>
-              <h2>Stephen Kilburn</h2>
-              <h2>Chief Operating Officer</h2>
+              <div className={styles.profile}>
+                <div className={styles.image}>
+                  <img
+                    src='/stuart.jpg'
+                    alt='Stuart Forsyth - Chief Executive Officer'
+                  />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <h2>Stuart Forsyth</h2>
+                <h2>Chief Executive Officer</h2>
+              </div>
+              <div className={styles.profile}>
+                <div className={styles.image}>
+                  <img
+                    src='/louise.jpg'
+                    alt='Louise Georgiou - Chief Finance Officer'
+                  />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <h2>Louise Georgiou</h2>
+                <h2>Chief Finance Officer</h2>
+              </div>
             </div>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img
-                  src='/stuart.jpg'
-                  alt='Stuart Forsyth - Chief Executive Officer'
-                />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
+            <div className={styles.row2}>
+              <div className={styles.profile}>
+                <div className={styles.image}>
+                  <img src='/mark.jpg' alt='Mark Pepper - Director' />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Mark Pepper</h2>
+                <h2>Director</h2>
               </div>
-              <h2>Stuart Forsyth</h2>
-              <h2>Chief Executive Officer</h2>
+              <div className={styles.profile}>
+                <div className={styles.image}>
+                  <img src='/shane.jpg' alt='Shane Richardson - Director' />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <h2>Shane Richardson</h2>
+                <h2>Director</h2>
+              </div>
             </div>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img
-                  src='/louise.jpg'
-                  alt='Louise Georgiou - Chief Finance Officer'
-                />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
+            <div className={styles.row2}>
+              <div className={styles.profile}>
+                <div className={styles.image}>
+                  <img src='/mark.jpg' alt='Neil Croft - Head of Operations' />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Neil Croft</h2>
+                <h2>Head of Operations</h2>
               </div>
-              <h2>Louise Georgiou</h2>
-              <h2>Chief Finance Officer</h2>
+              <div className={styles.profile}>
+                <div className={styles.image}>
+                  <img
+                    src='/arthur.jpg'
+                    alt='Arthur Grant - Consultant Head of Resources'
+                  />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
+                </div>
+                <h2>Arthur Grant</h2>
+                <h2>Consultant Head of Resources</h2>
+              </div>
             </div>
-          </div>
-          <div className={styles.row2}>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img src='/mark.jpg' alt='Mark Pepper - Director' />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
+            <div className={styles.supplementary}>
+              <div className={styles.suppProfile}>
+                <div className={styles.image}>
+                  <img src='/tim.jpg' alt='Tim Hughes - Credit Controller' />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Tim Hughes</h2>
+                <h2>Credit Controller</h2>
               </div>
-              <h2>Mark Pepper</h2>
-              <h2>Director</h2>
-            </div>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img src='/shane.jpg' alt='Shane Richardson - Director' />
-                <div className={styles.overlay}>
+              <div className={styles.suppProfile}>
+                <div className={styles.image}>
                   <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
+                    src='/harry.jpg'
+                    alt='Harry Forsyth - Commercial Executive'
                   />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Harry Forsyth</h2>
+                <h2>Commercial Executive</h2>
               </div>
-              <h2>Shane Richardson</h2>
-              <h2>Director</h2>
-            </div>
-          </div>
-          <div className={styles.row2}>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img src='/mark.jpg' alt='Neil Croft - Head of Operations' />
-                <div className={styles.overlay}>
+              <div className={styles.suppProfile}>
+                <div className={styles.image}>
                   <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
+                    src='/zoe.jpg'
+                    alt='Zoe Gallimore - Accounts Assistant'
                   />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Zoe Gallimore</h2>
+                <h2>Accounts Assistant</h2>
               </div>
-              <h2>Neil Croft</h2>
-              <h2>Head of Operations</h2>
-            </div>
-            <div className={styles.profile}>
-              <div className={styles.image}>
-                <img
-                  src='/arthur.jpg'
-                  alt='Arthur Grant - Consultant Head of Resources'
-                />
-                <div className={styles.overlay}>
+              <div className={styles.suppProfile}>
+                <div className={styles.image}>
                   <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
+                    src='/lovisa.jpg'
+                    alt='Lovisa Nygren - Logistics Controller'
                   />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Lovisa Nygren</h2>
+                <h2>Logistics Coordinator</h2>
               </div>
-              <h2>Arthur Grant</h2>
-              <h2>Consultant Head of Resources</h2>
-            </div>
-          </div>
-          <div className={styles.supplementary}>
-            <div className={styles.suppProfile}>
-              <div className={styles.image}>
-                <img src='/tim.jpg' alt='Tim Hughes - Credit Controller' />
-                <div className={styles.overlay}>
+              <div className={styles.suppProfile}>
+                <div className={styles.image}>
                   <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
+                    src='/polaris.jpg'
+                    alt='Polaris Zhang - Representative in China'
                   />
+                  <div className={styles.overlay}>
+                    <img
+                      className={styles.linkedinLogo}
+                      src='/random.svg'
+                      alt=''
+                    />
+                  </div>
                 </div>
+                <h2>Polaris Zhang</h2>
+                <h2>Representative in China</h2>
               </div>
-              <h2>Tim Hughes</h2>
-              <h2>Credit Controller</h2>
-            </div>
-            <div className={styles.suppProfile}>
-              <div className={styles.image}>
-                <img
-                  src='/harry.jpg'
-                  alt='Harry Forsyth - Commercial Executive'
-                />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
-                </div>
-              </div>
-              <h2>Harry Forsyth</h2>
-              <h2>Commercial Executive</h2>
-            </div>
-            <div className={styles.suppProfile}>
-              <div className={styles.image}>
-                <img src='/zoe.jpg' alt='Zoe Gallimore - Accounts Assistant' />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
-                </div>
-              </div>
-              <h2>Zoe Gallimore</h2>
-              <h2>Accounts Assistant</h2>
-            </div>
-            <div className={styles.suppProfile}>
-              <div className={styles.image}>
-                <img
-                  src='/lovisa.jpg'
-                  alt='Lovisa Nygren - Logistics Controller'
-                />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
-                </div>
-              </div>
-              <h2>Lovisa Nygren</h2>
-              <h2>Logistics Coordinator</h2>
-            </div>
-            <div className={styles.suppProfile}>
-              <div className={styles.image}>
-                <img
-                  src='/polaris.jpg'
-                  alt='Polaris Zhang - Representative in China'
-                />
-                <div className={styles.overlay}>
-                  <img
-                    className={styles.linkedinLogo}
-                    src='/random.svg'
-                    alt=''
-                  />
-                </div>
-              </div>
-              <h2>Polaris Zhang</h2>
-              <h2>Representative in China</h2>
             </div>
           </div>
         </div>

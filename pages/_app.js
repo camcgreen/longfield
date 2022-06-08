01da6e206cccc4cf3d032fdfd3e355f7d501f1a8
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-06-08 10:10:23 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <head>
+        <Script
+          strategy='lazyOnload'
+          src={`https://www.googletagmanager.com/gtag/js?id=G-89W6KNFWRV`}
+        />
+
+        <Script strategy='lazyOnload'>
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-89W6KNFWRV', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </Script>
+      </head>
       <Component {...pageProps} />
       <script
         src='//code.tidio.co/iare3ogxrz4hlfhlblobizor1lgqmehb.js'

cddba1e001ac3f1557ade3291ccde5ec8b7df989
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-10-14 15:25:15 +0100

    swap maps images and add locagreen logo to carousel

--- components/endorsements.js
+++ components/endorsements.js
@@ -3,142 +3,161 @@ import Autoplay from 'embla-carousel-autoplay';
 import styles from '../styles/Endorsements.module.css';
 
 const Endorsements = ({ lang }) => {
-  // const [carousel] = useEmblaCarousel({ loop: true });
-  const [carousel, emblaApi] = useEmblaCarousel({ loop: true }, [
-    Autoplay({ delay: 3000 }),
-  ]);
-  return (
-    <section className={styles.endorsements}>
-      <div className='wrapper'>
-        <div className={styles.carousel} ref={carousel}>
-          <div className={styles.carouselContainer}>
-            <div className={styles.carouselSlide}>
-              <div className={styles.carouselFlex}>
-                {/* <a
+    // const [carousel] = useEmblaCarousel({ loop: true });
+    const [carousel, emblaApi] = useEmblaCarousel({ loop: true }, [
+        Autoplay({ delay: 3000 }),
+    ]);
+    return (
+        <section className={styles.endorsements}>
+            <div className='wrapper'>
+                <div className={styles.carousel} ref={carousel}>
+                    <div className={styles.carouselContainer}>
+                        <div className={styles.carouselSlide}>
+                            <div className={styles.carouselFlex}>
+                                {/* <a
                   href='https://epsan.com/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-epsan.png' alt='Epsan' />
-                {/* </a> */}
-                {/* <a
+                                <img src='/c-epsan.png' alt='Epsan' />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.fpcusa.com/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-formosa-plastics.png' alt='Formosa Plastics' />
-                {/* </a> */}
-                {/* <a
+                                <img
+                                    src='/c-formosa-plastics.png'
+                                    alt='Formosa Plastics'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='http://www.fpc.com.tw/fpcw/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-formosa-corp.png' alt='Formosa Plastics Corp' />
-                {/* </a> */}
-                {/* <a
+                                <img
+                                    src='/c-formosa-corp.png'
+                                    alt='Formosa Plastics Corp'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.ineos-styrolution.com/index.html'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-ineos.jpg' alt='Ineos Styrolution' />
-                {/* </a> */}
-              </div>
-            </div>
-            <div className={styles.carouselSlide}>
-              <div className={styles.carouselFlex}>
-                {/* <a
+                                <img
+                                    src='/c-ineos.jpg'
+                                    alt='Ineos Styrolution'
+                                />
+                                {/* </a> */}
+                            </div>
+                        </div>
+                        <div className={styles.carouselSlide}>
+                            <div className={styles.carouselFlex}>
+                                {/* <a
                   href='https://inno-comp.hu/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img
-                  src='/c-inno.png'
-                  className={styles.inno}
-                  alt='Inno-Comp'
-                />
-                {/* </a> */}
-                {/* <a
+                                <img
+                                    src='/c-inno.png'
+                                    className={styles.inno}
+                                    alt='Inno-Comp'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.kkpc.com/eng/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-kumho.svg' alt='Kumho Petrochemical' />
-                {/* </a> */}
-                {/* <a
+                                <img
+                                    src='/c-kumho.svg'
+                                    alt='Kumho Petrochemical'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.lgchem.com/main/index'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-lg.svg' alt='LG Chem' />
-                {/* </a> */}
-                <img src='/c-locapol.svg' alt='Locapol' />
-              </div>
-            </div>
-            <div className={styles.carouselSlide}>
-              <div className={styles.carouselFlex}>
-                {/* <a
+                                <img src='/c-lg.svg' alt='LG Chem' />
+                                {/* </a> */}
+                                <img src='/c-locapol.svg' alt='Locapol' />
+                            </div>
+                        </div>
+                        <div className={styles.carouselSlide}>
+                            <div className={styles.carouselFlex}>
+                                {/* <a
                   href='https://longdamasterbatch.en.alibaba.com/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img
-                  src='/c-long.png'
-                  className={styles.long}
-                  alt='Long Da Masterbatch'
-                />
-                {/* </a> */}
-                {/* <a
+                                <img
+                                    src='/c-long.png'
+                                    className={styles.long}
+                                    alt='Long Da Masterbatch'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.lottechem.com/en/index.do'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-lotte.png' alt='Lotte Chemical' />
-                {/* </a> */}
-                {/* <a
+                                <img src='/c-lotte.png' alt='Lotte Chemical' />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.hip-petrohemija.com/home.8.html'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-petrohemija.jpeg' alt='Petrohemija' />
-                {/* </a> */}
-                {/* <a
+                                <img
+                                    src='/c-petrohemija.jpeg'
+                                    alt='Petrohemija'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.petkim.com.tr/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-petkim.svg' alt='Petkim' />
-                {/* </a> */}
-              </div>
-            </div>
-            <div className={styles.carouselSlide}>
-              <div className={styles.carouselFlex}>
-                {/* <a
+                                <img src='/c-petkim.svg' alt='Petkim' />
+                                {/* </a> */}
+                            </div>
+                        </div>
+                        <div className={styles.carouselSlide}>
+                            <div className={styles.carouselFlex}>
+                                {/* <a
                   href='https://www.sibur.ru/en/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                {/* <img src='/c-sibur.svg' alt='Sibur' /> */}
-                <img src='/c-locapol-reverse.svg' alt='Locapol' />
-                {/* </a> */}
-                {/* <a
+                                {/* <img src='/c-sibur.svg' alt='Sibur' /> */}
+                                <img
+                                    src='/c-locapol-reverse.svg'
+                                    alt='Locapol'
+                                />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.sidpec.com/'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-sidpec.png' alt='Sidpec' />
-                {/* </a> */}
-                {/* <a
+                                <img src='/c-sidpec.png' alt='Sidpec' />
+                                {/* </a> */}
+                                {/* <a
                   href='https://www.orlenunipetrol.cz/en/Pages/default.aspx'
                   target='_blank'
                   rel='noopener noreferrer'
                 > */}
-                <img src='/c-unipetrol.png' alt='Unipetrol' />
-                {/* </a> */}
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <div className={styles.grid}>
+                                <img src='/c-unipetrol.png' alt='Unipetrol' />
+                                {/* </a> */}
+                                <img src='/c-locagreen.svg' alt='Locagreen' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className={styles.grid}>
           <img src='/c-epsan.png' alt='Epsan' />
           <img
             src='/c-formosa-plastics.png'
@@ -161,14 +180,14 @@ const Endorsements = ({ lang }) => {
           <img src='/c-sidpec.png' alt='Sidpec' />
           <img src='/c-unipetrol.png' alt='Unipetrol' />
         </div> */}
-        <h1>
-          {lang === 'english'
-            ? 'These leading polymer producers choose Longfield'
-            : 'Empresas lideres en polímeros que escogen Longfield'}
-        </h1>
-      </div>
-    </section>
-  );
+                <h1>
+                    {lang === 'english'
+                        ? 'These leading polymer producers choose Longfield'
+                        : 'Empresas lideres en polímeros que escogen Longfield'}
+                </h1>
+            </div>
+        </section>
+    );
 };
 
 export default Endorsements;
--- pages/polymer-distribution.js
+++ pages/polymer-distribution.js
@@ -6,44 +6,44 @@ import Footer from '../components/footer';
 import Menu from '../components/menu';
 
 const PolymerDistribution = () => {
-  const [lang, setLang] = useState('english');
-  useEffect(() => {
-    const lang = localStorage.getItem('lang');
-    lang && setLang(lang);
-  }, []);
-  const handleCallback = (childData) => {
-    setLang(childData);
-  };
-  return (
-    <>
-      <Head>
-        <title>Polymer Distribution | Longfield Polymers</title>
-        <meta name='description' content='Longfield Polymers' />
-        <link rel='icon' href='/favicon.png' />
-      </Head>
-      <Header parentCallback={handleCallback} />
-      <main className={styles.content}>
-        <div className='wrapper'>
-          <img
-            src='/polymer-distribution-map.png'
-            alt='Polymer distribution map'
-          />
-          <h1>
-            {lang === 'english'
-              ? 'As a globally trusted polymer distributor, with supply partners all over the world, we are able to provide a reliable and efficient service for all your requirements.'
-              : 'Como distribuidor de polímeros de confianza a nivel mundial, con socios de suministro en todo el mundo, podemos ofrecer un servicio fiable y eficaz para todas sus necesidades.'}
-          </h1>
-          <p>
-            {lang === 'english'
-              ? 'Underpinned with considerable investment in technology, our efficient Europe-wide logistics network and strategically positioned UK distribution hubs ensure we deliver the right product in time at the right price.'
-              : 'Apoyados en una considerable inversión en tecnología, nuestra eficiente red de logística en toda Europa y los centros de distribución estrategicamente situados en el Reino Unido garantizan que entreguemos a nuestros clientes el producto adecuado a tiempo con un precio justo.'}
-          </p>
-        </div>
-      </main>
-      <Footer lang={lang} />
-      <Menu lang={lang} />
-    </>
-  );
+    const [lang, setLang] = useState('english');
+    useEffect(() => {
+        const lang = localStorage.getItem('lang');
+        lang && setLang(lang);
+    }, []);
+    const handleCallback = (childData) => {
+        setLang(childData);
+    };
+    return (
+        <>
+            <Head>
+                <title>Polymer Distribution | Longfield Polymers</title>
+                <meta name='description' content='Longfield Polymers' />
+                <link rel='icon' href='/favicon.png' />
+            </Head>
+            <Header parentCallback={handleCallback} />
+            <main className={styles.content}>
+                <div className='wrapper'>
+                    <img
+                        src='/polymer-distribution-map.png'
+                        alt='Polymer distribution map'
+                    />
+                    <h1>
+                        {lang === 'english'
+                            ? 'As a globally trusted polymer distributor, with supply partners all over the world, we are able to provide a reliable and efficient service for all your requirements.'
+                            : 'Como distribuidor de polímeros de confianza a nivel mundial, con socios de suministro en todo el mundo, podemos ofrecer un servicio fiable y eficaz para todas sus necesidades.'}
+                    </h1>
+                    <p>
+                        {lang === 'english'
+                            ? 'Underpinned with considerable investment in technology, our efficient Europe-wide logistics network and strategically positioned UK distribution hubs ensure we deliver the right product in time at the right price.'
+                            : 'Apoyados en una considerable inversión en tecnología, nuestra eficiente red de logística en toda Europa y los centros de distribución estrategicamente situados en el Reino Unido garantizan que entreguemos a nuestros clientes el producto adecuado a tiempo con un precio justo.'}
+                    </p>
+                </div>
+            </main>
+            <Footer lang={lang} />
+            <Menu lang={lang} />
+        </>
+    );
 };
 
 export default PolymerDistribution;
